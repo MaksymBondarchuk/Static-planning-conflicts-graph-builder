62ac1b8a7f78649d803491d810ab9169e4253477
MaxBondarchuk <bondarchuk.m.y@gmail.com>
2016-08-27 00:21:18 +0300

    Initial

--- Static planning conflicts graph builder/MainPage.xaml.cs
+++ Static planning conflicts graph builder/MainPage.xaml.cs
@@ -1,17 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -20,11 +14,93 @@ namespace Static_planning_conflicts_graph_builder
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class MainPage
     {
+        private Schedule Schedule { get; set; } = new Schedule();
+        private const int TestsCount = 100;
+
+        // Colors for graphs
+        private readonly List<Color> _colors = new List<Color>
+        {
+            Color.FromArgb(100, 153, 0, 0),
+            Color.FromArgb(100, 51, 102, 0),
+            Color.FromArgb(100, 0, 102, 102),
+            Color.FromArgb(100, 0, 51, 102),
+            Color.FromArgb(100, 76, 0, 153),
+            Color.FromArgb(100, 153, 0, 176),
+            Color.FromArgb(100, 32, 32, 32)
+        };
+
+        private int _colorIdx;
+
         public MainPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+        }
+
+        private async Task<List<Point>> DrawPoints(bool checkNecessary, int matrixSize)
+        {
+            var points = new List<Point>();
+
+            if (checkNecessary)
+                for (var connectivity = 0; connectivity <= 100; connectivity += 1)
+                {
+                    var possibility = .0;
+                    for (var testNumber = 0; testNumber < TestsCount; testNumber++)
+                        if (Schedule.CheckNecessary(matrixSize, connectivity))
+                            possibility++;
+                    possibility /= TestsCount;
+                    points.Add(new Point((int)(connectivity * CanvasGraph.RenderSize.Width * .01),
+                        (int)(CanvasGraph.RenderSize.Height - possibility * CanvasGraph.RenderSize.Height)));
+                    await Task.Delay(10);
+                }
+            else
+                for (var connectivity = 0; connectivity <= 100; connectivity += 1)
+                {
+                    var possibility = .0;
+                    for (var testNumber = 0; testNumber < TestsCount; testNumber++)
+                        if (Schedule.CheckConflict(matrixSize, connectivity))
+                            possibility++;
+                    possibility /= TestsCount;
+                    points.Add(new Point((int)(connectivity * CanvasGraph.RenderSize.Width * .01),
+                        (int)(CanvasGraph.RenderSize.Height - possibility * CanvasGraph.RenderSize.Height)));
+                    await Task.Delay(10);
+                }
+            return points;
+        }
+
+        private async void Build_Click(object sender, RoutedEventArgs e)
+        {
+            var points = await DrawPoints(CheckBoxType.IsChecked != null &&
+                CheckBoxType.IsChecked.Value, Convert.ToInt16(SliderSize.Value));
+
+            // Draw graph
+            var b = new SolidColorBrush(_colors[_colorIdx++ % _colors.Count]);
+            for (var i = 0; i < points.Count - 1; i++)
+            {
+                var line = new Line
+                {
+                    Stroke = b,
+                    X1 = points[i].X,
+                    X2 = points[i + 1].X,
+                    Y1 = points[i].Y,
+                    Y2 = points[i + 1].Y,
+                    StrokeThickness = 2
+                };
+
+                CanvasGraph.Children.Add(line);
+            }
+        }
+
+        private void ButtonClear_Click(object sender, RoutedEventArgs e)
+        {
+            _colorIdx = 0;
+            CanvasGraph.Children.Clear();
+        }
+
+        private void CanvasGraph_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            ButtonClear_Click(sender, null);
         }
     }
 }
---
+++ Static planning conflicts graph builder/Schedule.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Static_planning_conflicts_graph_builder
+{
+    public class Schedule
+    {
+        private List<List<short>> _matrix;
+        private readonly Random _rand = new Random();
+        /// <summary>
+        /// Generates random matrix with specified size and connectivity
+        /// </summary>
+        /// <param name="size">Size of the matrix</param>
+        /// <param name="connectivity">Connectivity of the matrix</param>
+        private void GenerateMarix(int size, int connectivity)
+        {
+            _matrix = new List<List<short>>();
+
+            for (var i = 0; i < size; i++)
+            {
+                _matrix.Add(new List<short>(size));
+                for (var j = 0; j < size; j++)
+                    _matrix[i].Add(0);
+            }
+
+
+            // Filling with ones
+            for (var i = 0; i < connectivity * size * size * .01; i++)
+            {
+                while (true)
+                {
+                    var x = _rand.Next(0, size);
+                    var y = _rand.Next(0, size);
+                    if (_matrix[x][y] != 0) continue;
+                    _matrix[x][y] = 1;
+                    break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Removes specified row and column from matrix
+        /// Addidtional for RemoveNecessary
+        /// </summary>
+        /// <param name="row">Row index</param>
+        /// <param name="col">Column index</param>
+        private void RemoveRowCol(int row, int col)
+        {
+            _matrix.RemoveAt(row);
+            foreach (var r in _matrix)
+                r.RemoveAt(col);
+        }
+
+        /// <summary>
+        /// Removes nessesary assignments
+        /// </summary>
+        private void RemoveNecessary()
+        {
+            for (var i = 0; i < _matrix.Count; i++)
+                for (var j = 0; j < _matrix.Count; j++)
+                    // If current cell is 1 and all others in row and column are zeros
+                    if (_matrix[i][j] == 1)
+                    {
+                        var countZerosCol = _matrix.Count(t => t[j] == 0);
+
+                        //for (var h = 0; h < _matrix.Count; h++)
+                        //    if (_matrix[h][j] == 0)
+                        //        countZerosCol++;
+
+                        //var countZerosCol = _matrix.Count(t => t[j] == 0);
+                        if (countZerosCol == _matrix.Count - 1)
+                        {
+                            RemoveRowCol(i, j);
+
+                            // Do the same with lesser matrix
+                            RemoveNecessary();
+                            return;
+                        }
+
+                        //var countZerosRow = _matrix.Where((t, j1) => _matrix[i][j1] == 0).Count();
+                        var countZerosRow = _matrix.Where((t, h) => _matrix[i][h] == 0).Count();
+                        // ReSharper disable once InvertIf
+                        if (countZerosRow == _matrix.Count - 1)
+                        {
+                            RemoveRowCol(i, j);
+
+                            // Do the same with lesser matrix
+                            RemoveNecessary();
+                            return;
+                        }
+                    }
+        }
+
+        /// <summary>
+        /// Swaps two rows in matrix
+        /// Additional for MakeZeroMatrixRecursive
+        /// </summary>
+        /// <param name="row1"></param>
+        /// <param name="row2"></param>
+        private void SwapRows(int row1, int row2)
+        {
+            var buf = _matrix[row1];
+            _matrix[row1] = _matrix[row2];
+            _matrix[row2] = buf;
+        }
+
+        /// <summary>
+        /// Swaps two columns in matrix
+        /// Additional for MakeZeroMatrixRecursive
+        /// </summary>
+        /// <param name="col1">First column index</param>
+        /// <param name="col2">Second column index</param>
+        private void SwapCols(int col1, int col2)
+        {
+            foreach (var t in _matrix)
+            {
+                var tmp = t[col1];
+                t[col1] = t[col2];
+                t[col2] = tmp;
+            }
+        }
+
+        /// <summary>
+        /// Tries to make zero matrix in top right corner
+        /// </summary>
+        /// <param name="offset">Offset in current matrix (0 at begin)</param>
+        private void MakeZeroMatrixRecursive(int offset)
+        {
+            if (offset == _matrix.Count)
+                return;
+
+            // Find row with min number of zeros
+            var imin = 0;
+            var min = _matrix.Count;
+            for (var i = offset; i < _matrix.Count; i++)
+            {
+                var currentOnes = 0;
+                for (var j = offset; j < _matrix.Count; j++)
+                    if (_matrix[i][j] == 1)
+                        currentOnes++;
+
+                if (currentOnes >= min) continue;
+                min = currentOnes;
+                imin = i;
+            }
+
+            // If found such row
+            if (offset < imin)
+                SwapRows(offset, imin);
+
+            // Find column with max number of ones where cell on "first" line is 1
+            var maxInitialised = false;
+            var jmax = 0;
+            var max = 0;
+            for (var j = 0; j < _matrix.Count; j++)
+                if (_matrix[offset][j] == 1)
+                {
+                    if (!maxInitialised)
+                    {
+                        jmax = j;
+                        maxInitialised = true;
+                    }
+
+                    var countOnes = 0;
+                    for (var i = offset; i < _matrix.Count; i++)
+                        if (_matrix[i][j] == 1)
+                            countOnes++;
+
+
+                    if (max >= countOnes) continue;
+                    max = countOnes;
+                    jmax = j;
+                }
+
+            // If found such row
+            if (offset < jmax)
+                SwapCols(offset, jmax);
+
+            // Do the same with lesser matrix
+            // ReSharper disable once TailRecursiveCall
+            MakeZeroMatrixRecursive(offset + 1);
+        }
+
+        /// <summary>
+        /// Checks matrix for conflict assignments
+        /// </summary>
+        /// <returns>True if there are conflicts. False else</returns>
+        private bool CheckConflict()
+        {
+            for (var i = 0; i < _matrix.Count; i++)
+                if (_matrix[i][i] == 1)
+                {
+                    var zerosNumber = 0;
+                    for (var i1 = 0; i1 <= i; i1++)
+                        for (var j1 = i; j1 < _matrix.Count; j1++)
+                            if (_matrix[i1][j1] == 0)
+                                zerosNumber++;
+                    if (zerosNumber == (i + 1) * (_matrix.Count - i) - 1)
+                        return true;
+                }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Generates and checks matrix for conflict assignments
+        /// </summary>
+        /// <param name="size">v</param>
+        /// <param name="connectivity">Connectivity of the matrix</param>
+        /// <returns>True if there are conflicts. False else</returns>
+        public bool CheckConflict(int size, int connectivity)
+        {
+            GenerateMarix(size, connectivity);
+            RemoveNecessary();
+            MakeZeroMatrixRecursive(0);
+            return CheckConflict();
+        }
+
+        /// <summary>
+        /// Generates and checks matrix for necessary assignments
+        /// </summary>
+        /// <param name="size">v</param>
+        /// <param name="connectivity">Connectivity of the matrix</param>
+        /// <returns>True if there are necessary assignments. False else</returns>
+        public bool CheckNecessary(int size, int connectivity)
+        {
+            GenerateMarix(size, connectivity);
+            var sizeBeforeRemoving = _matrix.Count;
+            RemoveNecessary();
+            return sizeBeforeRemoving != _matrix.Count;
+        }
+    }
+}
